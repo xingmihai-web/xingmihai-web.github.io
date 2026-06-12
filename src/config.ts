38f467f744a1a8c93c2c5dee02156ec6de5914b4
xingmihai-web <xingmihai@gmail.com>
2026-06-12 10:09:24 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,30 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: '星觅海的博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'https://xingmihai-web.github.io',
   // 网站副标题
-  Subtitle: '不曾与你分享的时间,我在进步.',
+  Subtitle: '一起慢慢进步.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: '星觅海的博客 专注于相关技术的实战分享。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
-  Author: '.𝙃𝙖𝙣',
+  Author: '𝑿𝑴𝑯𝑨𝑰',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640',
   // 网站座右铭
-  Motto: '运气是计划之外的东西.',
+  Motto: '除了死亡，一切失败都只存在于心理层面.',
   // Cover 网站缩略图
   Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
   // 网站侧边栏公告 (不填写即不开启)
   Tips: '<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>',
+// <p><img src=/assets/images/tips.gif style=width:100%></p>
   // 首页打字机文案列表
   TypeWriteList: [
-    '不曾与你分享的时间,我在进步.',
-    "I am making progress in the time I haven't shared with you.",
+    '一起慢慢进步.',
+    "Make progress together.",
   ],
   // 网站创建时间
-  CreateTime: '2021-09-01',
+  CreateTime: '2025-09-10',
   // 顶部 Banner 配置
   HomeBanner: {
     enable: true,
@@ -32,7 +33,9 @@ export default {
     // 其他页面高度
     PageHeight: '28.88rem',
     // 背景
-    background: "url('/assets/images/home-banner.webp') no-repeat center 60%/cover",
+    // https://pan.xmhai.cn/home-banner.webp
+    // /assets/images/home-banner.webp
+    background: "url('https://img.xmhai.cn/v2/YEqszPR.jpeg') no-repeat center 60%/cover",
   },
   // 博客主题配置
   Theme: {
@@ -53,23 +56,24 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: '朋友', link: '/links', icon: 'Nav_friends' },
-    { text: '圈子', link: '/friends', icon: 'Nav_rss' },
     { text: '动态', link: '/talking', icon: 'Nav_talking' },
     { text: '昔日', link: '/archives', icon: 'Nav_archives' },
     { text: '留言', link: '/message', icon: 'Nav_message' },
     { text: '关于', link: '/about', icon: 'Nav_about' },
-    { text: 'API', link: 'https://api.vvhan.com/', target: true, icon: 'Nav_link' },
   ],
   // 侧边栏个人网站
   WebSites: [
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
-    { text: 'Github', link: 'https://github.com/uxiaohan', icon: 'WebSite_github' },
-    { text: '韩小韩API', link: 'https://api.vvhan.com', icon: 'WebSite_api' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
-    { text: 'HanAnalytics', link: 'https://analytics.vvhan.com', icon: 'WebSite_analytics' },
-  ],
+    { text: 'Github', link: 'https://github.com/xingmih', icon: 'WebSite_github' },
+    { text: '骤雨重山图床', link: 'https://img.xmhai.cn', icon: 'WebSite_img' },
+    { text: 'Analytics', link: 'https://analytics.jasmiam.top', icon: 'WebSite_analytics' },
+    { text: 'MT论坛', link: 'https://bbs.binmt.cc/home.php?mod=space&uid=147108&do=profile', icon: 'WebSite_mt' },
+    { text: '微信', link: 'https://img.xmhai.cn/v2/ROIwNPT.png', icon: 'WebSite_wechat' },
+    { text: 'QQ', link: 'mqqopensdkapi://bizAgent/qm/qr?url=https%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Fk%3DtYrhpSkPVMGRrU_u6RBUcYwsXQAxh_b_%26jump_from%3D%26auth%3D%26app_name%3D%26authSig%3D%26source_id%3D3_40001', icon: 'WebSite_qq' },
+    { text: 'Email', link: 'mailto:hr@jasmiam.top', icon: 'WebSite_email' },
+    { text: 'bilibili', link: 'https://b23.tv/EscDwn5', icon: 'WebStie_bilibili' },
+    { text: 'QQ群', link: 'mqqapi://card/show_pslcard?src_type=internal&version=1&uin=1054810204&card_type=group&source=qrcode', icon: 'WebStie_group' },  ],
   // 侧边栏展示
   AsideShow: {
     // 是否展示个人网站
@@ -85,9 +89,7 @@ export default {
   DNSOptimization: [
     'https://i0.wp.com',
     'https://cn.cravatar.com',
-    'https://analytics.vvhan.com',
-    'https://vh-api.4ce.cn',
-    'https://registry.npmmirror.com',
+    'https://analytics.jasmiam.top',
     'https://pagead2.googlesyndication.com'
   ],
   // 博客音乐组件解析接口
@@ -98,15 +100,19 @@ export default {
     Twikoo: {
       enable: false,
       envId: ''
+//https://jasmiam.netlify.app
     },
     // Waline 评论
     Waline: {
-      enable: false,
-      serverURL: ''
+      enable: true,
+      serverURL: 'https://waline.xmhai.cn/.netlify/functions/comment'
+
+//https://waline.jasmiam.top/.netlify/functions/comment
+//https://waline2.jasmiam.top
     }
   },
   // Han Analytics 统计（https://github.com/uxiaohan/HanAnalytics）
-  HanAnalytics: { enable: true, server: 'https://analytics.vvhan.com', siteId: 'Hello-HanHexoBlog' },
+  Analytics: { enable: true, server: 'https://analytics.jasmiam.top', siteId: 'blog' },
   // Google 广告
   GoogleAds: {
     ad_Client: '', //ca-pub-xxxxxx
